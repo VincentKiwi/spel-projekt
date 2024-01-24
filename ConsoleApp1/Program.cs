c2580ebe30adea2f406ec2504ec9fec960530e62
VincentKiwi <vincent.kiwanuka@elev.ga.ntig.se>
2024-01-24 23:44:00 +0100

    lagt till alternative, ska lägga till spam

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,14 +4,17 @@
 Random dice = new Random();
 string player1Action = "";
 string mode = "";
+bool win;
+string[] actions = {"rock", "paper", "scissor"};
 
 // Introduction
 Console.WriteLine($"_-_-_-welcome to the game: rps spam -_-_-_");
 Console.WriteLine($"how the game works: the game is like rock papper scissors but in the result of a draw the game turns in to a spaming game");
 Console.WriteLine("the first one to take seven hp of the enemy or a player geats  KO (MEANING YOU WIN 3 ROUNDS IN A ROW)");
+Console.WriteLine("");
 
 // Player name
-Console.Write("What is player one's name?");
+Console.Write("What is player one's name? ");
 string player1 = Console.ReadLine();
 
 // Game mode
@@ -29,14 +32,41 @@ while (mode == "e")
 
     // Player action selection
     Console.Write("Do you pick rock, paper or scissor? ");
-    while (player1Action != "rock" || player1Action != "paper" || player1Action != "scissor"){
+    while (player1Action != "rock" && player1Action != "paper" && player1Action != "scissor"){
         player1Action = Console.ReadLine().ToLower();
     }
     
     // AI action selection
-    int AiAction = dice.Next(2);
-    
-    
+    string AiAction = actions[dice.Next(3)];
+
+    if (player1Action == "rock" && AiAction == "scissor")
+    {
+        win = true;
+    }
+    if (player1Action == "paper" && AiAction == "rock")
+    {
+        win = true;
+    }
+    if (player1Action == "scissor" && AiAction == "paper")
+    {
+        win = true;
+    }
+    if (player1Action == "scissor" && AiAction == "rock")
+    {
+        win = false;
+    }
+    if (player1Action == "rock" && AiAction == "paper")
+    {
+        win = false;
+    }
+    if (player1Action == "paper" && AiAction == "scissor")
+    {
+        win = false;
+    }
+    if (player1Action == AiAction)
+    {
+        
+    }
 }
 
 // Vs Player
